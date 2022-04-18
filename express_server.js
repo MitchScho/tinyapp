5be56a663b852901c6020a3d158e98a72ddedf82
MitchScho <scho.mitch@gmail.com>
2022-04-18 21:44:06 +0000

    create package for ejs

--- express_server.js
+++ express_server.js
@@ -6,6 +6,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -17,6 +19,15 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
 });
 
 app.listen(PORT, () => {
