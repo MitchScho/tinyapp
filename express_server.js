2cf55a20d0be1c85d0b9f49c0eb3668a1647b80e
MitchScho <scho.mitch@gmail.com>
2022-04-22 02:02:14 +0000

    find user urls function created

--- express_server.js
+++ express_server.js
@@ -74,7 +74,7 @@ const findUserByEmail = function (email) {
   return null; //user not found
 };
 
-
+    
 // POST req for user login
 app.post("/login", (req, res) => {
   const email = req.body.email;
@@ -106,15 +106,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-// POST request from client end to edit URLs
+// POST request from client to edit URLs
 app.post("/urls/:id", (req, res) => {
-  //rq.params.id is the data that comes from the input form. It's accessed and assigned to the 
-  //newShortURL variable
+  //rq.params.id is the data that comes from the input form. It's accessed and assigned to the newShortURL variable
   const newShortUrl = req.params.id;
-  //Then the newShortURL is used to update the req.body.longURL which is the edited URL from the client. 
-  //All is updated on the database
+  //Then the newShortURL is used to update the req.body.longURL which is the edited URL from the client. All is updated on the database
   urlDatabase[newShortUrl].longURL = req.body.longURL;
-  
+  const userID = req.cookies.userId;
+  const loggedInUserUrls = urlsForUserId(urlDatabase, userID);
+    if (!userID || !loggedInUserUrls) {
+      return res.status(401).send("SORRY: You must be logged in!");
+    }
   res.redirect(`/urls/${newShortUrl}`);
 });
   
@@ -138,7 +140,7 @@ const characters =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 const generateRandomString = function (length) {
-  let result = " ";
+  let result = "";
   const charactersLength = characters.length;
 
   //For loop is used to loop through the number passed into the generateRandomString() function
@@ -165,14 +167,29 @@ app.get("/login", (req, res) => {
   res.render("login", templateVars);
 });
 
+//Loops through database and finds shortURL objects that match the logged in user
+  const urlsForUserId = function (database, userId) {
+    const loggedInUserUrls = {};
+
+    for (let shortURL in database) {
+      if (database[shortURL].userID === userId) {
+        loggedInUserUrls[shortURL] = database[shortURL];
+      }
+    }
+    return loggedInUserUrls;
+  };
+
+
 //Get page /urls where list of urls is looped over and displayed
 app.get("/urls", (req, res) => {
   const loggedInUser = req.cookies.userId;
   if (!loggedInUser) {
     res.status(401).send("Please login to access Tinyapp.");
   }
-  const templateVars = { urls: urlDatabase, user: users[loggedInUser] };
-  console.log("index templatevars", templateVars);
+
+  const urlsForUser = urlsForUserId(urlDatabase, loggedInUser);
+  const templateVars = { urls: urlsForUser, user: users[loggedInUser] };
+  
   res.render("urls_index", templateVars);
 });
 
@@ -194,15 +211,18 @@ app.get("/urls/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   const loggedInUser = req.cookies.userId;
   const templateVars = { shortURL, longURL, user: users[loggedInUser] };
-  console.log("long url", longURL);
+  
   res.render("urls_show", templateVars );
 });
   
 
 //Get new route for shortURL requests. Where it redirects to the corresponding longURL
 app.get("/u/:shortURL", (req, res) => {
+  console.log("url database", urlDatabase);
   const longURL =  urlDatabase[req.params.shortURL].longURL;
-  
+  if (!longURL) {
+    res.status(401).send("Sorry: Incorrect URL");
+  }
   res.redirect(longURL);
 });
   
